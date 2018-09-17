c27206f4a97c7d9d099699bb366484e2430e69f5
Gannes Christofel <Kostas-96@yandex.ru>
2018-09-17 03:51:30 +0300

    Add functional

--- src/sample/PreviewCreater.java
+++ src/sample/PreviewCreater.java
@@ -1,16 +1,15 @@
 package sample;
 
-import com.sun.xml.internal.fastinfoset.util.StringArray;
-
 import java.lang.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 
 public class PreviewCreater {
 
     private static String[] line = new String[] {"https://www.autonews.ru/", "https://www.autonews.ru/news/5b8d1b449a79477e7db5e43a/",
             "https://www.rbc.ru/", "https://www.rbc.ru/economics/03/09/2018/5b86dbeb9a7947a42c448ca5/",
+            "http://tv.rbc.ru/", "http://tv.rbc.ru/archive/chez/5b9bdcf92ae5965edf641d34",
+            "https://www.rbc.ru/newspaper/", "https://www.rbc.ru/newspaper/2018/09/13/5b990d8b9a79472dad3da224",
+            "https://www.rbc.ru/magazine/", "https://www.rbc.ru/magazine/2018/09/5b76f3579a79472aed2d8cba",
             "https://quote.rbc.ru/", "https://quote.rbc.ru/news/article/5b8d420e9a79478a8ce28e28/",
             "https://sportrbc.ru/", "https://sportrbc.ru/news/5b8d39e79a7947881a6090cd/",
             "https://style.rbc.ru/", "https://style.rbc.ru/life/5b8929359a79473211c2cde9/",
@@ -18,6 +17,26 @@ public class PreviewCreater {
             "https://pink.rbc.ru/", "https://pink.rbc.ru/lifestyle/5b8998bb9a7947526bd5b704/",
             "http://zoom.cnews.ru/", "http://zoom.cnews.ru/publication/item/60892/"};
 
+    private static int myRand (String[] mass)
+    {
+        return new Random().nextInt(mass.length/2);
+    }
+
+    private static int myRand(int start, int end)
+    {
+        return new Random().nextInt(end - start +1) + start;
+    }
+
+    private static void contURL (String[] urls, int k) {
+        String arr[] = new String[line.length];
+        for (String aLine : line) {
+            for (int i = 0; i < line.length; i++)
+                if (aLine.contains(urls[i])) {
+                    k++;
+                    arr[k] = aLine;
+                }
+        }
+    }
 
     private static String onlyMob(String prw, String bannerType)
     {
@@ -29,8 +48,7 @@ public class PreviewCreater {
         switch (bannerType) {
             case "Billboard":
             case "240x400_left":
-                int p = new Random().nextInt((line.length - 2) / 2); //рандомная площадка (кроме Zoom)
-
+                int p = myRand(line); //рандомная площадка (-кроме Zoom)
                 if (p % 2 == 1) { return line[p - 1] + prw + "\n" + line[p] + prw + "\n"; }
                 else { return line[p] + prw + "\n" + line[p + 1] + prw + "\n"; }
 
@@ -61,7 +79,6 @@ public class PreviewCreater {
 
     private static String onlyTab(String prw, String bannerType)
     {
-
         String mass[] = new String[line.length];
         StringBuilder str = new StringBuilder();
         int k = 0;
@@ -89,6 +106,35 @@ public class PreviewCreater {
         return str.toString();
     }
 
+    private static String onlyPC(String prw, String bannerType)
+    {
+        String mass[] = new String[line.length];
+        StringBuilder str = new StringBuilder();
+        int k = 0;
+
+        //Биллборд
+        if (bannerType.equals("Billboard")) {
+            for (String aLine : line) {
+                if (aLine.contains("//www.rbc.ru") || aLine.contentEquals("https://tv.rbc.ru"))
+                { mass[k] = aLine; k++;}
+            }
+            int p = myRand(mass);
+            if (p % 2 == 1) { mass[0] = mass[p - 1]; mass[1] = mass[p]; k=2}
+            else { mass[0] = mass[p]; mass[1] = mass[p+1]; k=2}
+
+
+
+            for (String aLine : line) {
+                if (aLine.contains("style.rbc.ru") || aLine.contains("pink.rbc.ru"))
+                    { mass[k] = aLine; k++;}
+            }
+            int p = myRand(4, k);
+            if
+
+        }
+
+    }
+
 
     public static String checkBoxes(boolean[] Dvc, String prw, String bannerType) {
         //System.out.println("вошёл в функцию");
@@ -114,9 +160,9 @@ public class PreviewCreater {
             //ТОЛЬКО ПЛАНШЕТ
             if (Dvc[1] && !Dvc[0] && !Dvc[2]) {return onlyTab(prw, bannerType);}
 
-/*            //ТОЛЬКО ДЕСК
-            if (Dvc[0] && !Dvc[1] && !Dvc[2])*/
+/*            //ТОЛЬКО ДЕСК*/
+            if (Dvc[0] && !Dvc[1] && !Dvc[2]) {return onlyPC(prw, bannerType);}
 
-        else return "Пожалуйста, выберите устройство";
+        else return  "Пожалуйста, выберите устройство";
     }
 }
---
+++ src/sample/URLs.java
@@ -0,0 +1,6 @@
+package sample;
+
+public class URLs {
+    public static final URLs BILL =
+
+}
